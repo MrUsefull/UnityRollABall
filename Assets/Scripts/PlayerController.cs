d11c41e74bb2465d3987ec8e99dd64e356eae671
MrUsefull <sleepyjon0@gmail.com>
2017-11-05 14:35:05 -0800

    First commit likely last commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float speed;
+
+    private int score;
+
+    public Text scoreText;
+
+    public Text winnerText;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+        SetScoreText();
+        winnerText.gameObject.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHoriz = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHoriz,0, moveVert);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            ++score;
+            SetScoreText();
+        }
+        if (3 == score)
+        {
+            winnerText.gameObject.SetActive(true);
+        }
+    }
+
+    private void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
